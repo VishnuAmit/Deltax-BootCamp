a39be264301d8440ce53496988979aad3bb642bd
Vishnu Kasilya P <vishnukp21064@gmail.com>
2025-01-27 16:24:20 +0530

    used getresult method

--- csharp/Assignment3/IMDBApp/Exceptions/ MovieValidationException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace MovieApp.Exceptions
-{
-    public class MovieValidationException : Exception
-    {
-        public MovieValidationException(string message) : base(message) { }
-    }
-}
